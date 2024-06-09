80f9d706873124f213b914bd83817a97f7f6aeb5
Đặng Văn Tỵ <145207014+SnakeDang@users.noreply.github.com>
2024-06-09 22:41:17 +0700

    tess

--- start.go
+++ start.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "start server go neeee")
+        fmt.Fprint(w, "start server go neeee nooooo")
     })
 
     http.ListenAndServe(":3000", nil)
